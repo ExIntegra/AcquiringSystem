17ce66047058226262e4c628472409df8034ef0f
Ilya Kalinin <senpaihiroshimiyamoto@gmail.com>
2024-11-21 21:28:10 +0300

    Update RFID_Modul.ino
    Optimization

--- arduinoTerminal/RFID_Modul.ino
+++ arduinoTerminal/RFID_Modul.ino
@@ -7,8 +7,7 @@ String rfidCart()
   String uidString = "0x";                        // UID карты в формате HEX
   while(true)                                     // Бесконечный цикл, пока карта не будет считана
   {
-    if (!rfid.PICC_IsNewCardPresent()) continue;  // Если новая метка не поднесена - вернуться в начало while
-    if (!rfid.PICC_ReadCardSerial()) continue;    // Если метка не читается - вернуться в начало while
+    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) continue;  // Если новая метка не поднесена или метка не читается - вернуться в начало while
     for (int i = 0; i < 4; i++)                   // Если карта считана, то цикл пройдется по байтам и запишем в переменную uidString UID 
     {             
       uidString += String(rfid.uid.uidByte[i], HEX);
