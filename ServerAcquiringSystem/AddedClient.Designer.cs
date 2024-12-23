3363a608274b94c7c5c34ab116399abc9afa189f
ExIntegra <senpaihiroshimiyamoto@gmail.com>
2024-12-23 22:37:23 +0300

    Окно для добавления в базу данных информации о новом клиенте

---
+++ ServerAcquiringSystem/AddedClient.Designer.cs
@@ -0,0 +1,412 @@
+﻿namespace InterFaceModul
+{
+    partial class AddedClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            InfoForClientGroupBox = new GroupBox();
+            emailInput = new TextBox();
+            email = new Label();
+            phoneInput = new TextBox();
+            AgeInput = new TextBox();
+            DateOfBirthInput = new TextBox();
+            AdressInput = new TextBox();
+            pasportInput = new TextBox();
+            INNInput = new TextBox();
+            lastNameInput = new TextBox();
+            nameInput = new TextBox();
+            photoGroupBox = new GroupBox();
+            phoneLabel = new Label();
+            ageLabel = new Label();
+            DateOfBirthLabel = new Label();
+            AddressLabel = new Label();
+            pasportLabel = new Label();
+            INNLabel = new Label();
+            lastnameLabel = new Label();
+            nameLabel = new Label();
+            infoForAccountClient = new GroupBox();
+            textBox11 = new TextBox();
+            textBox10 = new TextBox();
+            textBox9 = new TextBox();
+            accounts = new GroupBox();
+            pincodeLabel = new Label();
+            balanceLabel = new Label();
+            AccountLabel = new Label();
+            addClientButton = new Button();
+            CancelClient = new Button();
+            middleNameInput = new TextBox();
+            middleNameLabel = new Label();
+            InfoForClientGroupBox.SuspendLayout();
+            infoForAccountClient.SuspendLayout();
+            SuspendLayout();
+            // 
+            // InfoForClientGroupBox
+            // 
+            InfoForClientGroupBox.Controls.Add(middleNameInput);
+            InfoForClientGroupBox.Controls.Add(middleNameLabel);
+            InfoForClientGroupBox.Controls.Add(emailInput);
+            InfoForClientGroupBox.Controls.Add(email);
+            InfoForClientGroupBox.Controls.Add(phoneInput);
+            InfoForClientGroupBox.Controls.Add(AgeInput);
+            InfoForClientGroupBox.Controls.Add(DateOfBirthInput);
+            InfoForClientGroupBox.Controls.Add(AdressInput);
+            InfoForClientGroupBox.Controls.Add(pasportInput);
+            InfoForClientGroupBox.Controls.Add(INNInput);
+            InfoForClientGroupBox.Controls.Add(lastNameInput);
+            InfoForClientGroupBox.Controls.Add(nameInput);
+            InfoForClientGroupBox.Controls.Add(photoGroupBox);
+            InfoForClientGroupBox.Controls.Add(phoneLabel);
+            InfoForClientGroupBox.Controls.Add(ageLabel);
+            InfoForClientGroupBox.Controls.Add(DateOfBirthLabel);
+            InfoForClientGroupBox.Controls.Add(AddressLabel);
+            InfoForClientGroupBox.Controls.Add(pasportLabel);
+            InfoForClientGroupBox.Controls.Add(INNLabel);
+            InfoForClientGroupBox.Controls.Add(lastnameLabel);
+            InfoForClientGroupBox.Controls.Add(nameLabel);
+            InfoForClientGroupBox.Location = new Point(12, 12);
+            InfoForClientGroupBox.Name = "InfoForClientGroupBox";
+            InfoForClientGroupBox.Size = new Size(503, 568);
+            InfoForClientGroupBox.TabIndex = 5;
+            InfoForClientGroupBox.TabStop = false;
+            InfoForClientGroupBox.Text = "Данные о клиенте";
+            // 
+            // emailInput
+            // 
+            emailInput.Location = new Point(111, 495);
+            emailInput.Name = "emailInput";
+            emailInput.Size = new Size(189, 31);
+            emailInput.TabIndex = 25;
+            // 
+            // email
+            // 
+            email.AutoSize = true;
+            email.Location = new Point(24, 495);
+            email.Name = "email";
+            email.Size = new Size(54, 25);
+            email.TabIndex = 24;
+            email.Text = "email";
+            // 
+            // phoneInput
+            // 
+            phoneInput.Location = new Point(106, 444);
+            phoneInput.Name = "phoneInput";
+            phoneInput.Size = new Size(189, 31);
+            phoneInput.TabIndex = 23;
+            // 
+            // AgeInput
+            // 
+            AgeInput.Location = new Point(106, 397);
+            AgeInput.Name = "AgeInput";
+            AgeInput.Size = new Size(48, 31);
+            AgeInput.TabIndex = 22;
+            // 
+            // DateOfBirthInput
+            // 
+            DateOfBirthInput.Location = new Point(170, 349);
+            DateOfBirthInput.Name = "DateOfBirthInput";
+            DateOfBirthInput.Size = new Size(151, 31);
+            DateOfBirthInput.TabIndex = 21;
+            // 
+            // AdressInput
+            // 
+            AdressInput.Location = new Point(92, 306);
+            AdressInput.Name = "AdressInput";
+            AdressInput.Size = new Size(392, 31);
+            AdressInput.TabIndex = 20;
+            // 
+            // pasportInput
+            // 
+            pasportInput.Location = new Point(135, 263);
+            pasportInput.Name = "pasportInput";
+            pasportInput.Size = new Size(186, 31);
+            pasportInput.TabIndex = 19;
+            // 
+            // INNInput
+            // 
+            INNInput.Location = new Point(135, 212);
+            INNInput.Name = "INNInput";
+            INNInput.Size = new Size(186, 31);
+            INNInput.TabIndex = 18;
+            // 
+            // lastNameInput
+            // 
+            lastNameInput.Location = new Point(135, 112);
+            lastNameInput.Name = "lastNameInput";
+            lastNameInput.Size = new Size(186, 31);
+            lastNameInput.TabIndex = 17;
+            // 
+            // nameInput
+            // 
+            nameInput.Location = new Point(135, 64);
+            nameInput.Name = "nameInput";
+            nameInput.Size = new Size(186, 31);
+            nameInput.TabIndex = 5;
+            // 
+            // photoGroupBox
+            // 
+            photoGroupBox.Location = new Point(355, 30);
+            photoGroupBox.Name = "photoGroupBox";
+            photoGroupBox.Size = new Size(142, 150);
+            photoGroupBox.TabIndex = 16;
+            photoGroupBox.TabStop = false;
+            photoGroupBox.Text = "Фото";
+            // 
+            // phoneLabel
+            // 
+            phoneLabel.AutoSize = true;
+            phoneLabel.Location = new Point(24, 447);
+            phoneLabel.Name = "phoneLabel";
+            phoneLabel.Size = new Size(81, 25);
+            phoneLabel.TabIndex = 14;
+            phoneLabel.Text = "Телефон";
+            // 
+            // ageLabel
+            // 
+            ageLabel.AutoSize = true;
+            ageLabel.Location = new Point(24, 400);
+            ageLabel.Name = "ageLabel";
+            ageLabel.Size = new Size(76, 25);
+            ageLabel.TabIndex = 6;
+            ageLabel.Text = "Возраст";
+            // 
+            // DateOfBirthLabel
+            // 
+            DateOfBirthLabel.AutoSize = true;
+            DateOfBirthLabel.Location = new Point(24, 352);
+            DateOfBirthLabel.Name = "DateOfBirthLabel";
+            DateOfBirthLabel.Size = new Size(140, 25);
+            DateOfBirthLabel.TabIndex = 5;
+            DateOfBirthLabel.Text = "День рождения";
+            // 
+            // AddressLabel
+            // 
+            AddressLabel.AutoSize = true;
+            AddressLabel.Location = new Point(24, 306);
+            AddressLabel.Name = "AddressLabel";
+            AddressLabel.Size = new Size(62, 25);
+            AddressLabel.TabIndex = 4;
+            AddressLabel.Text = "Адрес";
+            // 
+            // pasportLabel
+            // 
+            pasportLabel.AutoSize = true;
+            pasportLabel.Location = new Point(24, 263);
+            pasportLabel.Name = "pasportLabel";
+            pasportLabel.Size = new Size(81, 25);
+            pasportLabel.TabIndex = 3;
+            pasportLabel.Text = "Паспорт";
+            // 
+            // INNLabel
+            // 
+            INNLabel.AutoSize = true;
+            INNLabel.Location = new Point(24, 218);
+            INNLabel.Name = "INNLabel";
+            INNLabel.Size = new Size(51, 25);
+            INNLabel.TabIndex = 2;
+            INNLabel.Text = "ИНН";
+            // 
+            // lastnameLabel
+            // 
+            lastnameLabel.AutoSize = true;
+            lastnameLabel.Location = new Point(20, 112);
+            lastnameLabel.Name = "lastnameLabel";
+            lastnameLabel.Size = new Size(85, 25);
+            lastnameLabel.TabIndex = 1;
+            lastnameLabel.Text = "Фамилия";
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new Point(24, 64);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new Size(47, 25);
+            nameLabel.TabIndex = 0;
+            nameLabel.Text = "Имя";
+            // 
+            // infoForAccountClient
+            // 
+            infoForAccountClient.Controls.Add(textBox11);
+            infoForAccountClient.Controls.Add(textBox10);
+            infoForAccountClient.Controls.Add(textBox9);
+            infoForAccountClient.Controls.Add(accounts);
+            infoForAccountClient.Controls.Add(pincodeLabel);
+            infoForAccountClient.Controls.Add(balanceLabel);
+            infoForAccountClient.Controls.Add(AccountLabel);
+            infoForAccountClient.Location = new Point(558, 20);
+            infoForAccountClient.Name = "infoForAccountClient";
+            infoForAccountClient.Size = new Size(477, 568);
+            infoForAccountClient.TabIndex = 6;
+            infoForAccountClient.TabStop = false;
+            infoForAccountClient.Text = "Информация о счете";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(103, 60);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(186, 31);
+            textBox11.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(103, 138);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(186, 31);
+            textBox10.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(103, 101);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(186, 31);
+            textBox9.TabIndex = 6;
+            // 
+            // accounts
+            // 
+            accounts.Location = new Point(36, 311);
+            accounts.Name = "accounts";
+            accounts.Size = new Size(421, 243);
+            accounts.TabIndex = 4;
+            accounts.TabStop = false;
+            accounts.Text = "Другие счета:";
+            // 
+            // pincodeLabel
+            // 
+            pincodeLabel.AutoSize = true;
+            pincodeLabel.Location = new Point(22, 138);
+            pincodeLabel.Name = "pincodeLabel";
+            pincodeLabel.Size = new Size(75, 25);
+            pincodeLabel.TabIndex = 3;
+            pincodeLabel.Text = "Пинкод";
+            // 
+            // balanceLabel
+            // 
+            balanceLabel.AutoSize = true;
+            balanceLabel.Location = new Point(24, 100);
+            balanceLabel.Name = "balanceLabel";
+            balanceLabel.Size = new Size(67, 25);
+            balanceLabel.TabIndex = 2;
+            balanceLabel.Text = "Баланс";
+            // 
+            // AccountLabel
+            // 
+            AccountLabel.AutoSize = true;
+            AccountLabel.Location = new Point(24, 60);
+            AccountLabel.Name = "AccountLabel";
+            AccountLabel.Size = new Size(49, 25);
+            AccountLabel.TabIndex = 1;
+            AccountLabel.Text = "Счет";
+            // 
+            // addClientButton
+            // 
+            addClientButton.Location = new Point(339, 613);
+            addClientButton.Name = "addClientButton";
+            addClientButton.Size = new Size(112, 34);
+            addClientButton.TabIndex = 7;
+            addClientButton.Text = "Добавить";
+            addClientButton.UseVisualStyleBackColor = true;
+            addClientButton.Click += addClientButton_Click;
+            // 
+            // CancelClient
+            // 
+            CancelClient.Location = new Point(519, 613);
+            CancelClient.Name = "CancelClient";
+            CancelClient.Size = new Size(112, 34);
+            CancelClient.TabIndex = 8;
+            CancelClient.Text = "Отмена";
+            CancelClient.UseVisualStyleBackColor = true;
+            CancelClient.Click += CancelClient_Click;
+            // 
+            // middleNameInput
+            // 
+            middleNameInput.Location = new Point(131, 162);
+            middleNameInput.Name = "middleNameInput";
+            middleNameInput.Size = new Size(186, 31);
+            middleNameInput.TabIndex = 27;
+            // 
+            // middleNameLabel
+            // 
+            middleNameLabel.AutoSize = true;
+            middleNameLabel.Location = new Point(20, 165);
+            middleNameLabel.Name = "middleNameLabel";
+            middleNameLabel.Size = new Size(88, 25);
+            middleNameLabel.TabIndex = 26;
+            middleNameLabel.Text = "Отчество";
+            // 
+            // AddedClient
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1107, 659);
+            Controls.Add(CancelClient);
+            Controls.Add(addClientButton);
+            Controls.Add(infoForAccountClient);
+            Controls.Add(InfoForClientGroupBox);
+            Name = "AddedClient";
+            Text = "AddedClient";
+            InfoForClientGroupBox.ResumeLayout(false);
+            InfoForClientGroupBox.PerformLayout();
+            infoForAccountClient.ResumeLayout(false);
+            infoForAccountClient.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox InfoForClientGroupBox;
+        private GroupBox photoGroupBox;
+        private Label phoneLabel;
+        private Label ageLabel;
+        private Label DateOfBirthLabel;
+        private Label AddressLabel;
+        private Label pasportLabel;
+        private Label INNLabel;
+        private Label lastnameLabel;
+        private Label nameLabel;
+        private GroupBox infoForAccountClient;
+        private GroupBox accounts;
+        private Label pincodeLabel;
+        private Label balanceLabel;
+        private Label AccountLabel;
+        private TextBox phoneInput;
+        private TextBox AgeInput;
+        private TextBox DateOfBirthInput;
+        private TextBox AdressInput;
+        private TextBox pasportInput;
+        private TextBox INNInput;
+        private TextBox lastNameInput;
+        private TextBox nameInput;
+        private TextBox textBox11;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private Button addClientButton;
+        private Button CancelClient;
+        private TextBox emailInput;
+        private Label email;
+        private TextBox middleNameInput;
+        private Label middleNameLabel;
+    }
+}
