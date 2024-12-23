fa3e6fb1c1acebcf0d1e2dad1481303099a1e168
ExIntegra <senpaihiroshimiyamoto@gmail.com>
2024-12-23 22:38:48 +0300

    -

---
+++ ServerAcquiringSystem/Requisites.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaceModul
+{
+    internal class Requisites
+    {
+        public int PersonID { get; }
+        public int AccountID { get; }
+        public int Pincode { get; }
+        public int Account { get; }
+        public int Balalce { get; }
+
+        public Requisites(int balance, int account)
+        {
+            this.Balalce = balance;
+            this.Account = account;
+        }
+    }
+}
