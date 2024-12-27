f8627b8b9dba6022900b53aefd98efe2f2be3d13
ExIntegra <senpaihiroshimiyamoto@gmail.com>
2024-12-28 01:58:20 +0300

    Подключение EntityFramework

---
+++ ServerAcquiringSystem/database/db.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace InterFaceModul
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
