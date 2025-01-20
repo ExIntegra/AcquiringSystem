b2dffcc0c8cf864bd5263c86735a6bc117cde7e2
ExIntegra <senpaihiroshimiyamoto@gmail.com>
2025-01-20 13:01:56 +0300

    Удаление лишних состояний автомата и его оптимизация.

--- arduinoTerminal/automatonOfStates.ino
+++ arduinoTerminal/automatonOfStates.ino
@@ -2,72 +2,57 @@ void PaymentProcess:: automatonOfStates()
 {
   while(currentState != terminalStatus::EXIT)
   {
-    //Serial.println("Current State: " + getCurrentStateString());
+    Serial.println("Current State: " + getCurrentStateString());
     switch(currentState)
     {
       case terminalStatus::CONNECT_TERMINAL_TO_SERVER:
-        currentState = terminalStatus::WAITING_INPUT_PRICE;
-        comPortConnect();
+        ComPortConnect();
+        break;
+      
+      case terminalStatus::MENU:
+        Menu();
         break;
 
       case terminalStatus::WAITING_INPUT_PRICE:
-        currentState = terminalStatus::WAITING_CARD;
         displayMessage("RUB", true, 13, 1);                         
-        displayMessage("K oplate", false, 0, 0);                
+        displayMessage("To be paid:", false, 0, 0);     
+
+        currentState = terminalStatus::WAITING_CARD;
         setPrice(keyboard());
         break;
 
       case terminalStatus::WAITING_CARD:
-        displayMessage("Go card", true, 0, 0);
-        rfidCard();
         currentState = terminalStatus::INPUT_PINCODE;
+        rfidCard();
         break;
       
       case terminalStatus::INPUT_PINCODE:
-        currentState = terminalStatus::PROCESSING_PAYMENT;
         displayMessage("Input pincode:", true, 0, 0);
+
+        currentState = terminalStatus::PROCESSING_PAYMENT;
         setPincode(keyboard());
         break;
 
       case terminalStatus::PROCESSING_PAYMENT:
-        processPayment(price, uid);
+        currentState = terminalStatus::MENU;
+        processPayment();
         break;
-
-      case terminalStatus::PAYMENT_SUCCESS:
-        displayMessage("Payment", true, 0, 0);                 
-        displayMessage("successful!", false, 2, 0);
-        currentState = terminalStatus::EXIT;
-        break;
-      
-      case terminalStatus::CARD_BLOCKED:
-        displayMessage("Card is block", true, 0, 0);
-        currentState = terminalStatus::EXIT;
       
       case terminalStatus::INVALID_PINCODE:
-        displayMessage("invalid pincode", true, 0, 0);
-        currentState = terminalStatus::WAITING_INPUT_PRICE;
-        break;
-
-      case terminalStatus::NO_MONEY:
-        displayMessage("No money", true, 0, 0);
-        currentState = terminalStatus::EXIT;
-        break;
-      
-      case terminalStatus::ERROR_SERVER:
-        displayMessage("Error server", true, 0, 0);
-        currentState = terminalStatus::EXIT;
+        currentState = terminalStatus::INPUT_PINCODE;
         break;
 
       case terminalStatus::EXIT:
         displayMessage("EXIT", true, 0, 0);
+        delay(3000);
         break;
 
       default:
         displayMessage("Error terminal", true, 0, 0);
+        delay(5000);
         currentState = terminalStatus::EXIT;
         break;
     }
   }
-  //Serial.println("Current State: EXIT");
-  displayMessage("ENDING JOB", true, 0, 0);
+  currentState = terminalStatus::MENU;
 }
