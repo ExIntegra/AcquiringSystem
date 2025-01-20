78f34644fe8230edf8728964b6ff1a3e0f56835c
ExIntegra <senpaihiroshimiyamoto@gmail.com>
2025-01-20 13:00:26 +0300

    Временное упрощение sett-еров для упрощения отладки

--- arduinoTerminal/settersMethodes.ino
+++ arduinoTerminal/settersMethodes.ino
@@ -1,40 +1,14 @@
 void PaymentProcess::setUID(String uid)
 {
-  uint8_t maxLengthUID = 8;
-
-  if(uid.length() == maxLengthUID){
-    this->uid = uid;
-  }
-
-  else{
-    displayMessage("Error uid card", true, 0, 0);
-    delay(3000);
-    currentState = terminalStatus::WAITING_INPUT_PRICE;
-    }
+  this->uid = uid;
 }
 ////////////////////////////////////////////////////////////////////////////
-void PaymentProcess::setPrice(int price)
+void PaymentProcess::setPrice(String price)
 {
-  if(price > 0 || isDigit1(price)){
-    this->price = price;
-  }
-            
-  else{
-    displayMessage("Error input price", true, 0, 0);
-    delay(3000);
-    currentState = terminalStatus::WAITING_INPUT_PRICE;
-  }  
+  this->price = price;
 }
 ////////////////////////////////////////////////////////////////////////////
-void PaymentProcess::setPincode(int pincode)
+void PaymentProcess::setPincode(String pincode)
 {
-  if(pincode > 0 || isDigit1(pincode)){
-    this->pincode = pincode;
-  }
-
-  else{
-    displayMessage("Error set pincode", true, 0, 0);
-    delay(3000);
-    currentState = terminalStatus::WAITING_INPUT_PRICE;
-    }
+  this->pincode = pincode;
 }
